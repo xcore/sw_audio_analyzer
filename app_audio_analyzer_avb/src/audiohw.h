273b512dd01065eb71114df56e12f5d3c93f29f5
Peter Hedinger <peter@xmos.com>
2014-01-06 11:56:19 +0000

    Adding AVB configuration parameters.

--- app_audio_analyzer_avb/src/audiohw.h
+++ app_audio_analyzer_avb/src/audiohw.h
@@ -3,6 +3,11 @@
 
 #define AUDIO_IO_TILE 0
 
+#define AUDIO_SETTLE_IGNORE_COUNT         100000
+#define AUDIO_SIGNAL_DETECT_THRESHOLD   15000000
+#define AUDIO_NOISE_FLOOR                     15
+
+
 void AudioHwInit(void);
 void genclock(void);
 
