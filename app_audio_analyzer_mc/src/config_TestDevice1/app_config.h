ec7eaf28f49d122dd2efb3895e7f339fa655d174
David Lacey <david.lacey@xmos.com>
2014-01-07 09:32:28 +0000

    Reset app configs freqs

--- app_audio_analyzer_mc/src/config_TestDevice1/app_config.h
+++ app_audio_analyzer_mc/src/config_TestDevice1/app_config.h
@@ -2,10 +2,10 @@
 #define I2S_MASTER_NUM_CHANS_DAC 4
 
 #define CHAN_CONFIG \
- {{ SINE, 500, 0, 0 }, \
-  { SINE, 1000, 0, 0 }, \
-  { SINE, 1500, 0, 0 }, \
-  { SINE, 2000, 0, 0 } \
+ {{ SINE, 1000, 0, 0 }, \
+  { SINE, 2000, 0, 0 }, \
+  { SINE, 3000, 0, 0 }, \
+  { SINE, 4000, 0, 0 } \
  }
 
 #define BASE_CHAN_ID       0
--- app_audio_analyzer_mc/src/config_TestDevice2/app_config.h
+++ app_audio_analyzer_mc/src/config_TestDevice2/app_config.h
@@ -2,10 +2,10 @@
 #define I2S_MASTER_NUM_CHANS_DAC 4
 
 #define CHAN_CONFIG \
- {{ SINE, 2500, 0, 0 }, \
-  { SINE, 3000, 0, 0 }, \
-  { SINE, 3500, 0, 0 }, \
-  { SINE, 4000, 0, 0 } \
+ {{ SINE, 5000, 0, 0 }, \
+  { SINE, 6000, 0, 0 }, \
+  { SINE, 7000, 0, 0 }, \
+  { SINE, 8000, 0, 0 } \
  }
 
 #define BASE_CHAN_ID       4
