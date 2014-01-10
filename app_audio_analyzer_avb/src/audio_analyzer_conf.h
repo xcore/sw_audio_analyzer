18c82191196559ee7f009ca337067879bab5532a
Peter Hedinger <peter@xmos.com>
2014-01-10 11:07:17 +0000

    Increase the AVB signal threshold.

--- app_audio_analyzer_avb/src/audio_analyzer_conf.h
+++ app_audio_analyzer_avb/src/audio_analyzer_conf.h
@@ -1,6 +1,6 @@
 #ifndef __audio_analyzer_conf_h__
 #define __audio_analyzer_conf_h__
 
-#define AUDIO_SIGNAL_DETECT_THRESHOLD   10000000
+#define AUDIO_SIGNAL_DETECT_THRESHOLD   30000000
 
 #endif // __audio_analyzer_conf_h__
