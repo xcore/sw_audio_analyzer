1d2c22884cb4110b0621b23f1c6903a3655e7e18
David Lacey <david.lacey@xmos.com>
2014-01-12 08:25:08 +0000

    Remove configs from mc app and merge in upstream changes to that app

--- app_audio_analyzer_mc/src/config_GlitchInjector/app_config.h
+++
@@ -1,9 +0,0 @@
-#define I2S_MASTER_NUM_CHANS_ADC 4
-#define I2S_MASTER_NUM_CHANS_DAC 4
-
-#define CHAN_CONFIG \
- {{ 1, SINE, 1000, 0, 0, 0 }, \
-  { 1, SINE, 2000, -1, 0, 59321 }, \
-  { 1, SINE, 3000, -1, 0, 4345 }, \
-  { 1, SINE, 4000, 0, 0, 0 } \
- }
