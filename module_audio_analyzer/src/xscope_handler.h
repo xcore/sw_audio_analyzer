7c316975cff5a33e9fd4e4cacbf419684f7c3f47
ed-xmos <ed@xmos.com>
2014-02-12 14:41:40 +0000

    Added plumbing for SPDIF ramp generator

--- module_audio_analyzer/src/xscope_handler.h
+++ module_audio_analyzer/src/xscope_handler.h
@@ -2,6 +2,7 @@
 #define __xscope_handler_h__
 
 #include "signal_gen.h"
+#include "ramp_gen.h"
 #include "audio_analyzer.h"
 #include "app_config.h"
 
@@ -17,6 +18,7 @@
 void xscope_handler(chanend c_host_data,
     client interface error_flow_control_if i_flow_control,
     client interface channel_config_if i_chan_config,
+    client interface spdif_config_if ?i_spdif_config,
     client interface analysis_control_if i_control[n], unsigned n);
 
 void error_reporter(server interface error_flow_control_if i_flow_control,
